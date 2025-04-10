7718e987ebbdca04e252173d61127b59d587e6f8
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-04-10 22:10:15 +0100

    Delete all packages

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
 
-from statistics import mode
+from statistics import mode, median
     
 
 class MyApp(QWidget):
@@ -12,6 +12,7 @@ class MyApp(QWidget):
 
         self.label = QLabel("Enter marks:")
         self.mode_label = QLabel("")
+        self.median_label = QLabel("")
         self.entry = QLineEdit()
         self.button = QPushButton("Submit")
 
@@ -20,18 +21,20 @@ class MyApp(QWidget):
         layout.addWidget(self.entry)
         layout.addWidget(self.button)
         layout.addWidget(self.mode_label)
+        layout.addWidget(self.median_label)
 
         self.setLayout(layout)
-        self.button.clicked.connect(self.numbers_mode)
+        self.button.clicked.connect(self.calculate_marks)
 
-    def numbers_mode(self):
-        mode_number = 0;
+    def calculate_marks(self):
         student_marks = self.entry.text()
         list_of_marks = [float(mark.strip()) for mark in student_marks.replace(',', ' ').split()]
         mode_number = mode(list_of_marks)
-        self.mode_label.setText(f"Mode: {mode_number}") 
+        median_number = median(list_of_marks)
+        self.mode_label.setText(f"Mode: {mode_number}")
+        self.median_label.setText(f"Median: {median_number}")  
         self.entry.clear()
-
+       
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyApp()
