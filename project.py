901ed1431e0ba411c9957cffd2a1d42ab3acdf51
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-04-10 21:25:14 +0100

    Add mode functionality

--- project.py
+++ project.py
@@ -1,13 +1,17 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
 
+from statistics import mode
+    
+
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Numbers Input")
-        self.setGeometry(100, 100, 300, 150)
+        self.setGeometry(100, 100, 100, 150)
 
-        self.label = QLabel("Enter numbers:")
+        self.label = QLabel("Enter marks:")
+        self.mode_label = QLabel("")
         self.entry = QLineEdit()
         self.button = QPushButton("Submit")
 
@@ -15,13 +19,17 @@ class MyApp(QWidget):
         layout.addWidget(self.label)
         layout.addWidget(self.entry)
         layout.addWidget(self.button)
+        layout.addWidget(self.mode_label)
 
         self.setLayout(layout)
-        self.button.clicked.connect(self.numbers_submit)
+        self.button.clicked.connect(self.numbers_mode)
 
-    def numbers_submit(self):
-        numbers = self.entry.text()
-        print("You entered:", numbers)
+    def numbers_mode(self):
+        mode_number = 0;
+        student_marks = self.entry.text()
+        list_of_marks = [float(mark.strip()) for mark in student_marks.replace(',', ' ').split()]
+        mode_number = mode(list_of_marks)
+        self.mode_label.setText(f"Mode: {mode_number}") 
         self.entry.clear()
 
 if __name__ == "__main__":
