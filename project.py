694f531ff40a7931527617b1622690c8587dd015
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-04-11 15:34:40 +0100

    add method for finding mean

--- project.py
+++ project.py
@@ -1,42 +1,14 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
+def findMean(marks):
+        sorted_marks = sorted(marks)
+        mean = 0
+        sum = 0
+        for mark in sorted_marks:
+            sum += mark
+        mean = sum / len(marks)
+        
+        return mean
 
-from statistics import mode, median
-    
 
-class MyApp(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Numbers Input")
-        self.setGeometry(100, 100, 100, 150)
+print("Enter student marks one at a time. Type 'end' when you want to stop the program or if you want to start again type back.")
 
-        self.label = QLabel("Enter marks:")
-        self.mode_label = QLabel("")
-        self.median_label = QLabel("")
-        self.entry = QLineEdit()
-        self.button = QPushButton("Submit")
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.label)
-        layout.addWidget(self.entry)
-        layout.addWidget(self.button)
-        layout.addWidget(self.mode_label)
-        layout.addWidget(self.median_label)
-
-        self.setLayout(layout)
-        self.button.clicked.connect(self.calculate_marks)
-
-    def calculate_marks(self):
-        student_marks = self.entry.text()
-        list_of_marks = [float(mark.strip()) for mark in student_marks.replace(',', ' ').split()]
-        mode_number = mode(list_of_marks)
-        median_number = median(list_of_marks)
-        self.mode_label.setText(f"Mode: {mode_number}")
-        self.median_label.setText(f"Median: {median_number}")  
-        self.entry.clear()
-       
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MyApp()
-    window.show()
-    sys.exit(app.exec_())
