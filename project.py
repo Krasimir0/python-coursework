98d092af433790a1b568a3650119904d3991eec3
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-04-11 22:04:26 +0100

    Add method to find mode

--- project.py
+++ project.py
@@ -12,16 +12,34 @@ def findMedian(marks):
         sorted_marks = sorted(marks)
         sorted_marks_length = len(sorted_marks) 
         median = 0
-        sum = 0
+
         if sorted_marks_length % 2 == 0:
                 mid1 = sorted_marks[sorted_marks_length // 2 - 1]
                 mid2 = sorted_marks[sorted_marks_length // 2]
                 median = (mid1 + mid2) / 2
-                print(median)
         else:
                 median = sorted_marks[sorted_marks_length // 2]
-        
+
         return median
+
+def findMode(marks):
+        marksObj = {}
+        for mark in marks:
+            if mark in marksObj:
+                    marksObj[mark] += 1
+            else:
+                   marksObj[mark] = 1
+        
+        max_count = max(marksObj.values())
+
+        modes = [mark for mark, count in marksObj.items() if count == max_count]
+
+        if len(modes) == 1:
+                return modes[0]
+        else:
+                return modes
+        
+                
 
 print("Enter student marks one at a time. Type 'end' when you want to stop the program or if you want to start again type back.")
 
@@ -37,4 +55,8 @@ while True:
                 continue
     mean = findMean(user_input_marks)
     median = findMedian(user_input_marks)
+    mode = findMode(user_input_marks)
+    print(mean)
+    print(median)
+    print(mode)
 
