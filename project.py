e9443902811ff2247d5b2b9f19e45b9fc0932256
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-04-11 15:35:07 +0100

    Add method to find median

--- project.py
+++ project.py
@@ -8,6 +8,20 @@ def findMean(marks):
         
         return mean
 
+def findMedian(marks):
+        sorted_marks = sorted(marks)
+        sorted_marks_length = len(sorted_marks) 
+        median = 0
+        sum = 0
+        if sorted_marks_length % 2 == 0:
+                mid1 = sorted_marks[sorted_marks_length // 2 - 1]
+                mid2 = sorted_marks[sorted_marks_length // 2]
+                median = (mid1 + mid2) / 2
+                print(median)
+        else:
+                median = sorted_marks[sorted_marks_length // 2]
+        
+        return median
 
 print("Enter student marks one at a time. Type 'end' when you want to stop the program or if you want to start again type back.")
 
